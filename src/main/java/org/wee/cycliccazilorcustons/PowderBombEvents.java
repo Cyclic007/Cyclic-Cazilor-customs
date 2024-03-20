498946a8720243974bbf1e99ca0327a9d2a5ae5e
Cyclic007 <99688870+Cyclic007@users.noreply.github.com>
2024-03-20 14:11:17 -0500

    update

---
+++ src/main/java/org/wee/cycliccazilorcustons/PowderBombEvents.java
@@ -0,0 +1,52 @@
+package org.wee.cycliccazilorcustons;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.projectiles.ProjectileSource;
+
+public class PowderBombEvents implements Listener {
+    @EventHandler
+    public void letHimCook(ProjectileLaunchEvent event) {
+        if (event.getEntity() instanceof ThrownPotion) {
+            /*  60 */
+            Entity source = (Entity) event.getEntity().getShooter();
+            ProjectileSource projectileSource = event.getEntity().getShooter();
+            /*  61 */
+            assert source != null;
+            if (source.getType() == EntityType.PLAYER) {
+                Player player = (Player) source;
+                /*  62 */
+                ItemMeta itemMeta = player.getItemInHand().getItemMeta();
+                /*  63 */
+                if (itemMeta != null && itemMeta.hasDisplayName() && itemMeta.getDisplayName().contains("§6Light Powder Bag")) {
+                    /*  64 */
+                    event.getEntity().setMetadata("Explosive", new FixedMetadataValue((Plugin) this, Boolean.valueOf(true)));
+                    /*     */
+                }
+                /*  66 */
+                if (itemMeta != null && itemMeta.hasDisplayName() && itemMeta.getDisplayName().contains("§6Powder Bag")) {
+                    /*  67 */
+                    event.getEntity().setMetadata("Explosive2", new FixedMetadataValue((Plugin) this, Boolean.valueOf(true)));
+                    /*     */
+                }
+                /*     */
+
+
+
+
+                /*     */
+            }
+
+        }
+    }
+}
+
+
---
+++ src/main/java/org/wee/cycliccazilorcustons/TODO.md
@@ -0,0 +1,1 @@
+assert meta != null;
