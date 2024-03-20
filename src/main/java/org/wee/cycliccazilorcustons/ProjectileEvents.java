885798fd4ae60b8a518b12303a274939adc3d137
Cyclic007 <99688870+Cyclic007@users.noreply.github.com>
2024-03-20 13:00:39 -0500

    added the effects for drinking

--- src/main/java/org/wee/cycliccazilorcustons/ProjectileEvents.java
+++ src/main/java/org/wee/cycliccazilorcustons/ProjectileEvents.java
@@ -1,25 +1,18 @@
 package org.wee.cycliccazilorcustons;
 
-import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.PotionSplashEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.generator.BiomeProvider;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
@@ -83,288 +76,116 @@ import java.util.logging.Logger;
                 }
                 /*     */
             }
-            /*  59 */
-            if (event.getEntity() instanceof ThrownPotion) {
-                /*  60 */
-                Player player = (Player) event.getEntity().getShooter();
-                /*  61 */
-                if (player.getItemInHand() != null) {
-                    /*  62 */
-                    ItemMeta itemMeta = player.getItemInHand().getItemMeta();
-                    /*  63 */
-                    if (itemMeta != null && itemMeta.hasDisplayName() && itemMeta.getDisplayName().contains("§6Light Powder Bag")) {
-                        /*  64 */
-                        event.getEntity().setMetadata("Explosive", new FixedMetadataValue(this, Boolean.valueOf(true)));
-                        /*     */
-                    }
-                    /*  66 */
-                    if (itemMeta != null && itemMeta.hasDisplayName() && itemMeta.getDisplayName().contains("§6Powder Bag")) {
-                        /*  67 */
-                        event.getEntity().setMetadata("Explosive2", new FixedMetadataValue(this, Boolean.valueOf(true)));
-                        /*     */
-                    }
-                    /*     */
-                }
-                /*     */
-            }
-            /*     */
         }
-        /*     */
     }
 
-    /*     */
-    /*     */
-    @EventHandler
-    /*     */ public void onHit(EntityDamageByEntityEvent event) {
-        /*  76 */
-        Entity damager = event.getDamager();
-        /*  77 */
-        if (!(damager instanceof org.bukkit.entity.Arrow))
-            /*  78 */ return;
-        if (damager.hasMetadata("TaneArrow")) {
-            /*  79 */
-            event.setDamage(30.0D);
-            /*     */
-        }
-        /*  81 */
-        if (damager.hasMetadata("BlunderArrow")) {
-            /*  82 */
-            event.setDamage(3.5D);
-            /*     */
-        }
-        /*     */
-    }
-
-    /*     */
-    /*     */
-    @EventHandler
-    /*     */ public void onBlockHit(ProjectileHitEvent event) {
-        /*  88 */
-        Projectile p = event.getEntity();
-        /*  89 */
-        if (p instanceof org.bukkit.entity.Arrow &&
-                /*  90 */       p.hasMetadata("BlunderArrow")) {
-            /*  91 */
-            p.remove();
-            /*     */
-        }
-        /*     */
-    }
-
-    /*     */
-    /*     */
-    /*     */
-    @EventHandler
-    /*     */ public void onPotionSplash(PotionSplashEvent event) {
-        /*  98 */
-        ThrownPotion potion = event.getEntity();
-        /*  99 */
-        if (potion.hasMetadata("Explosive")) {
-            /* 100 */
-            Location potionLocation = event.getEntity().getLocation();
-            /* 101 */
-            event.getEntity().getWorld().createExplosion(potionLocation.getX(), potionLocation.getY(), potionLocation.getZ(), 1.5F, false, true);
-            /*     */
-        }
-        /* 103 */
-        if (potion.hasMetadata("Explosive2")) {
-            /* 104 */
-            Location potionLocation = event.getEntity().getLocation();
-            /* 105 */
-            event.getEntity().getWorld().createExplosion(potionLocation.getX(), potionLocation.getY(), potionLocation.getZ(), 2.5F, false, true);
-            /*     */
-        }
-        /*     */
-    }
-
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public File getDataFolder() {
-        /* 111 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public PluginDescriptionFile getDescription() {
-        /* 116 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public FileConfiguration getConfig() {
-        /* 121 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public InputStream getResource(String s) {
-        /* 126 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void saveConfig() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void saveDefaultConfig() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void saveResource(String s, boolean b) {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void reloadConfig() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public PluginLoader getPluginLoader() {
-        /* 151 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public Server getServer() {
-        /* 156 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public boolean isEnabled() {
-        /* 161 */
         return false;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void onDisable() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void onLoad() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void onEnable() {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public boolean isNaggable() {
-        /* 181 */
         return false;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public void setNaggable(boolean b) {
+
     }
 
-    /*     */
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public ChunkGenerator getDefaultWorldGenerator(String s, String s1) {
-        /* 191 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public BiomeProvider getDefaultBiomeProvider(String s, String s1) {
-        /* 196 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public Logger getLogger() {
-        /* 201 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public String getName() {
-        /* 206 */
         return null;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        /* 211 */
         return false;
-        /*     */
     }
 
-    /*     */
-    /*     */
-    /*     */
+    @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
-        /* 216 */
         return null;
-        /*     */
     }
-    /*     */
 }
