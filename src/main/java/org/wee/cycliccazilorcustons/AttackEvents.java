f03b476a3866bee9b73ecc7ac1e28ab7fbc30287
Cyclic007 <99688870+Cyclic007@users.noreply.github.com>
2024-03-20 16:08:39 -0500

    more event handleing

--- src/main/java/org/wee/cycliccazilorcustons/AttackEvents.java
+++ src/main/java/org/wee/cycliccazilorcustons/AttackEvents.java
@@ -1,4 +1,27 @@
 package org.wee.cycliccazilorcustons;
 
-public class AttackEvents {
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+public class AttackEvents implements Listener {
+    @EventHandler
+    public static void attackEvent(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        Vector face = player.getFacing().getDirection();
+        ItemStack item = player.getItemInUse();
+        Location loc = face.toLocation(player.getWorld());
+
+        if (item == ItemManager.whip && player.isHandRaised()) {
+            face.multiply(5);
+            Vector left = face.clone();
+            Vector right = face.clone();
+        }
+    }
 }
